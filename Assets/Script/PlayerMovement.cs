4866a4e0d2164d43109fae8f6bc246edcce0a89c
whdgur2498 <whdgur2498@gmail.com>
2024-11-13 21:22:30 +0900

    [Add] PlayerMovment

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("PlayerMovement Option")]
+    [SerializeField] public float moveSpeed = 5f; // 이동 속도
+    [SerializeField] public float moveInterval = 2f; // 이동 간격 (초)
+    [SerializeField] public Vector2 minPosition; // 이동 가능한 최소 위치
+    [SerializeField] public Vector2 maxPosition; // 이동 가능한 최대 위치
+
+    private Vector3 targetPosition;
+    private Animator animator; // Animator 컴포넌트
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        // 일정 시간마다 MoveToRandomPosition 함수를 호출합니다.
+        InvokeRepeating("MoveToRandomPosition", 0f, moveInterval);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // 플레이어를 목표 위치로 이동시킵니다.
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        if (transform.position != targetPosition) 
+        { 
+            animator.SetBool("isWalking", true); 
+        } 
+        else 
+        { 
+            animator.SetBool("isWalking", false); 
+        }
+    }
+
+    private void MoveToRandomPosition()
+    {
+        // 랜덤한 목표 위치를 설정합니다.
+        float randomX = Random.Range(minPosition.x, maxPosition.x);
+        float randomY = Random.Range(minPosition.y, maxPosition.y);
+        targetPosition = new Vector3(randomX, randomY, transform.position.z);
     }
 }
